c3610c3dbd709f018c18c1f49c47691ee769065f
Jimmy Garcia <jimmy.garcia@etu.unice.fr>
2026-01-26 17:35:35 +0100

    Dernière version

--- src/config/db.php
+++ src/config/db.php
@@ -6,12 +6,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 if (!defined('DB_HOST')) {
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'gj402456_game_hub'); // Vérifie bien le nom de ta BDD ici
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
+    define('DB_HOST', 'linserv-info-01');
+    define('DB_NAME', 'se401325_gamehub'); // Vérifie bien le nom de ta BDD ici
+    define('DB_USER', 'se401325');
+    define('DB_PASS', 'se401325');
 }
 
 try {
